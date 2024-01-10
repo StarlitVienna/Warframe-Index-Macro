a350b3fbdc7ff2c54097531f20e6485d7f78937d
StarlitVienna <86577602+StarlitVienna@users.noreply.github.com>
2024-01-10 14:06:27 -0300

    Update IndexMacro.sh

--- IndexMacro.sh
+++ IndexMacro.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#xdotool windowactivate --sync 18874411 key --clearmodifiers --delay 1000
-#xdotool windowactivate --sync 18874411
 win=$(xdotool getactivewindow)
-WarframeWindowId=65011713
+WarframeWindowId=000000000
 xdotool keyup ctrl
 for ((x=0; x<1000000; x++)); do
 xdotool windowactivate --sync $WarframeWindowId key --clearmodifiers --delay 5 --window $WarframeWindowId y
@@ -51,8 +49,6 @@ xdotool windowactivate --sync $WarframeWindowId key --clearmodifiers --delay 500
 xdotool windowactivate --sync $WarframeWindowId key --clearmodifiers --delay 5 --window $WarframeWindowId y
 xdotool keyup y
 xdotool keyup ctrl
-#xdotool windowactivate --sync 65011713 key --clearmodifiers --delay 500 --window 65011713 5
-#mousemove 100 100 click 3
 xdotool keyup 5
 xdotool keyup ctrl
 sleep 1.5;
